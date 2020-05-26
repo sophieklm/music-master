7f9e0a626dd357af539df62c6146ad35e4be5bbd
Sophie Mann <sklm@live.co.uk>
2020-05-26 17:58:40 +0100

    Get spotify auth and request to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FormGroup, FormControl, InputGroup, Button } from 'react-bootstrap';
+import request from 'request';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,8 +10,43 @@ class App extends React.Component {
     };
   }
 
-  search() {
-    console.log('this.state', this.state);
+  async search() {
+    const client_id = process.env.REACT_APP_CLIENT_ID;
+    const client_secret = process.env.REACT_APP_CLIENT_SECRET;
+
+    const auth = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        grant_type: 'client_credentials',
+      },
+      json: true,
+      headers: {
+        Authorization:
+          'Basic ' +
+          new Buffer(client_id + ':' + client_secret).toString('base64'),
+      },
+    };
+
+    const BASE_URL = 'https://api.spotify.com/v1/search';
+    const FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+
+    request.post(auth, async (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        // use the access token to access the Spotify Web API
+        var token = body.access_token;
+        var options = {
+          url: FETCH_URL,
+          headers: {
+            Authorization: 'Bearer ' + token,
+          },
+          json: true,
+        };
+        request.get(options, (error, response, body) => {
+          const artist = body.artists.items[0];
+          this.setState({ artist });
+        });
+      }
+    });
   }
 
   render() {
