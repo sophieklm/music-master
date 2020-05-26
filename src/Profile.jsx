2603830250929f10b2e6252f0b56a483ee6c6b2f
Sophie Mann <sklm@live.co.uk>
2020-05-26 22:13:37 +0100

    Style and add README

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -10,17 +10,21 @@ class Profile extends React.Component {
             className="profile-image"
             src={this.props.artist.images[0].url}
           />
-          <div>{this.props.artist.name}</div>
-          <div>{this.props.artist.followers.total}</div>
-          <div>
-            {this.props.artist.genres.map((genre, k) => {
-              const genres = this.props.artist.genres;
-              genre =
-                genre !== genres[genres.length - 1]
-                  ? ` ${genre},`
-                  : ` ${genre}`;
-              return <span key={k}>{genre}</span>;
-            })}
+          <div className="profile-info">
+            <div className="profile-name">{this.props.artist.name}</div>
+            <div className="profile-followers">
+              Followers: {this.props.artist.followers.total}
+            </div>
+            <div className="profile-genres">
+              {this.props.artist.genres.map((genre, k) => {
+                const genres = this.props.artist.genres;
+                genre =
+                  genre !== genres[genres.length - 1]
+                    ? ` ${genre},`
+                    : ` ${genre}`;
+                return <span key={k}>{genre}</span>;
+              })}
+            </div>
           </div>
         </div>
       );
